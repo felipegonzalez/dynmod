c128b05c317e1758af6779409f4f3abfb026e555
Felipe González <felipexgonzalez@gmail.com>
2012-07-26 18:22:04 -0500

    Agregar modelo de market share de PWH

---
+++ src/prueba_mshare.R
@@ -0,0 +1,52 @@
+library(dlm)
+
+y <- share$SHARE
+plot(y, type='o')
+
+X.1 <- share[, c('PROM','CPROM','PRICE')]
+#X.1$relprom.s <- X.1$relprom /sd(X.1$relprom)
+#X.1$relprom <- NULL
+
+plot(X.1$PRICE)
+#plot(X.1$relprom.s, type="l")
+plot(share$promdiff)
+mod.2 <- unclass(
+	dlmModPoly(1, dV = 0, m0=42, C0 = (5^2) )+ 
+	dlmModReg(X = X.1,  addInt=FALSE, C0 = (2)^2 * diag(3))
+	)
+
+mod.2$k <- 1
+mod.2$n0 <- 1
+mod.2$d0 <- 1
+
+
+salida.f <- dlm.filt(y, mod.2, bloques=list(1, 2:4), 
+	descuento=c(0.98,0.95), delta=0.99)
+salida <- salida.f$filter
+
+
+sd.1 <- sqrt(unlist(salida$n)*unlist(salida$Q)/(unlist(salida$n)-2))
+plot(y, type='p', ylim=c(38,45))
+lines(sapply(salida$f, function(el){el[1,1]}), col=2, type='l')
+lines(1.7*sd.1+  sapply(salida$f, function(el){el[1,1]}), col=2, type='l',lty=3)
+lines(-1.7*sd.1+  sapply(salida$f, function(el){el[1,1]}), col=2, type='l',lty=3)
+
+
+plot(y, type='o')
+lines(sapply(salida$a, function(el){el[1,1]}), col=2, type='l')
+
+acf(unlist(salida$e))
+
+
+## Nivel suavizado
+salida.s <- dlm.smooth(salida.f)
+a.suave <- sapply(salida.s$a.smooth, function(el){el[1,1]})
+sd.a <- sqrt(sapply(salida.s$R.smooth, function(el){el[1,1]}))
+plot(y, type='o')
+lines(a.suave, col=2, type='l')
+lines(a.suave-1.7*sd.a, col=2, type='l')
+lines(a.suave+1.7*sd.a, col=2, type='l')
+
+
+
+
